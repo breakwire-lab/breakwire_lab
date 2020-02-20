c177e3e800697f50c0739d925666525b803fdb1a
Chen Jian <lsdlab@icloud.com>
2020-02-20 22:00:40 +0800

    update

--- publish.sh
+++ publish.sh
@@ -1,5 +1,9 @@
 #! /bin/zsh
 
+git add .
+git commit -m "update"
+git push origin master
+
 ssh root@139.162.87.225 "cd breakwire_lab; git pull origin master; exit"
 
 echo "Site updated at https://lab.breakwire.me"
